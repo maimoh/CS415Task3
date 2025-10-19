f0287cbab3f8060ff12911c387cf5c1f4691be7b
maimoh <maimoh85349@gmail.com>
2025-10-19 17:26:20 +0300

    first commit

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    void testAddition() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    void testSubtraction() {
+        Calculator calc = new Calculator();
+        assertEquals(1, calc.subtract(3, 2));
+    }
+
+    @Test
+    void testDivision() {
+        Calculator calc = new Calculator();
+        assertEquals(2, calc.divide(6, 3));
+    }
+
+    @Test
+    void testDivideByZero() {
+        Calculator calc = new Calculator();
+        assertThrows(IllegalArgumentException.class, () -> calc.divide(5, 0));
+    }
+}
