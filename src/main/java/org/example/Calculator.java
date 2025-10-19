b8bb797de40fc98c0391d057cc2af0a855000b14
maimoh <maimoh85349@gmail.com>
2025-10-19 22:11:50 +0300

    change expected value

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,22 +2,22 @@ package org.example;
 
 public class Calculator {
 
-    // دالة بسيطة للجمع
+
     public int add(int a, int b) {
         return a + b;
     }
 
-    // دالة للطرح
+
     public int subtract(int a, int b) {
         return a - b;
     }
 
-    // دالة للضرب
+
     public int multiply(int a, int b) {
         return a * b;
     }
 
-    // دالة للقسمة
+
     public int divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Cannot divide by zero!");
